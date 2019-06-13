83e4b52741105d5c5ab49859febea411d5e232b2
zengkaiwei2018 <1912207369@qq,com>
2019-06-13 11:10:05 +0800

      我的第一个程序hello world

---
+++ textcpp/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  textcpp
+//
+//  Created by s20181105306 on 2019/6/12.
+//  Copyright © 2019 s20181105306. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    //gfygygjhgjhjhgkg
+    return 0;
+}
